b71d5189395734e235445adc57bf8a5056e59da7
Cherry <79985974+chaeri93@users.noreply.github.com>
2022-07-13 02:57:35 +0900

    delete db

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'this is secret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:aa06030137@localhost:3306/piuda?charset=utf8'
+app.config['SQLALCHEMY_DATABASE_URI'] = '
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
